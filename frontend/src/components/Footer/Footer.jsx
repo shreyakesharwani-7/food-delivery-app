ab720edd16f918b2e6b14887aa3f43de72ed4685
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-08-07 21:19:11 +0530

    Initial commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem lpsum is simply dummy text of the printing and typesetting industry. Loream lpsum has been the indusrty's standarddummy text ever since the 1500s, when an unknown printer took a gally of type and scrambled it to make a type specimen book.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+   <div className="footer-content-centre">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>                
+                </ul>
+            </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">
+            © 2026 Tomato.com - All Rights Reserved.
+            </p>
+    </div>
+  )
+}
+
+export default Footer
