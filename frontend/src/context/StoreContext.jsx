f93e5d019e7fc0a0c61e9e7bda7967c931a0d31c
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-08-23 22:52:23 +0530

    All items attaching with dataBase

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,17 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider =(props)=>{
 
     const [cartItems,setCartItems]= useState({});
+
+
     const url = "http://localhost:4000"    
     const [token, setToken] = useState("");
+
+    const [food_list,setFoodList] = useState([])
 
 
     const addToCart=(itemId)=>{
@@ -34,7 +38,23 @@ const StoreContextProvider =(props)=>{
         }
         return totalAmount;
     }
-    
+
+    const fetchFoodList = async()=>{
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+
+useEffect(()=>{
+
+async function loadData(){
+    await fetchFoodList();
+    if (localStorage.getItem("token")) {
+    setToken(localStorage.getItem("token"));
+}
+}
+loadData();
+},[])
+
 
     const contextValue={
         food_list,
