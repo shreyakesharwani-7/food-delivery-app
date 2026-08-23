7d66ab0d65445533c44bda56b39e1a891dcb3d50
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-08-23 22:36:16 +0530

    signIn ans logout functionlity adding

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,6 +6,9 @@ export const StoreContext = createContext(null)
 const StoreContextProvider =(props)=>{
 
     const [cartItems,setCartItems]= useState({});
+    const url = "http://localhost:4000"    
+    const [token, setToken] = useState("");
+
 
     const addToCart=(itemId)=>{
         if(!cartItems[itemId]){
@@ -39,7 +42,10 @@ const StoreContextProvider =(props)=>{
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return(
